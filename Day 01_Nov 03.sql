24807da10f9da2eaa61438ecd74c9743c2356eef
Nithya Dharshini <122954701+Nithya-Dharshini@users.noreply.github.com>
2025-11-03 14:48:18 +0000

    Add files via upload

---
+++ Day 01_Nov 03.sql
@@ -0,0 +1,24 @@
+-- Practice Qns
+
+-- Q1.Retrieve all columns from the patients table
+select * 
+from patients;
+
+-- Q2. Select only the patient_id, name, and age columns from the patients table
+select patient_id, name, age
+from patients;
+
+-- Q3. Display the first 10 records from the services_weekly table
+select *
+from services_weekly
+limit 10;
+
+
+-- Daily Challenge
+
+-- List all unique hospital services available in the hospital
+select distinct service 
+from services_weekly;
+
+
+
