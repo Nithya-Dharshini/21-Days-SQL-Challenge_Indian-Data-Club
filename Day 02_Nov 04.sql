b80da2a8fa8232ce454717735e6a91440fc074a3
Nithya Dharshini <122954701+Nithya-Dharshini@users.noreply.github.com>
2025-11-04 15:09:44 +0000

    Update Day 02_Nov 04.sql

--- Day 02_Nov 04.sql
+++ Day 02_Nov 04.sql
@@ -25,4 +25,4 @@ where patients_request>100;
 -- Find all patients admitted to 'Surgery' service with a satisfaction score below 70, showing their patient_id, name, age, and satisfaction score.
 select patient_id, name, age, satisfaction
 from patients
-where service="surgery" AND satisfaction>70;
+where service="surgery" AND satisfaction<70;
